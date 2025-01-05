0e5420c4cecb379ff7b527e126b3ea9acc0b2887
Minister <mundestephane13@gmail.com>
2025-01-06 00:50:43 +0100

    fixed issue with map id and advanced marker

--- src/app/common/services/map.service.ts
+++ src/app/common/services/map.service.ts
@@ -15,23 +15,31 @@ export class MapService {
       region: 'CM',
       ...options,
     });
-    console.log('loader ready', this.loader);
   }
 
   initMap(element: HTMLElement, options?: google.maps.MapOptions) {
     this.loader.importLibrary('maps').then(({ Map }) => {
       this.map = new Map(element, {
+        mapId: google.maps.Map.DEMO_MAP_ID,
         ...options,
       });
     });
   }
 
-  async addMarker(
-    options: Omit<google.maps.marker.AdvancedMarkerElementOptions, 'map'>,
+  async addAdvancedMarker(
+    options: Omit<google.maps.marker.AdvancedMarkerElementOptions, 'map'> & {
+      pin?: google.maps.marker.PinElementOptions;
+    },
   ) {
     return this.loader
       .importLibrary('marker')
       .then(({ AdvancedMarkerElement, PinElement }) => {
+        if (options.pin) {
+          options.content = new PinElement({ ...options.pin }).element;
+        }
+
+        delete options.pin;
+
         return new AdvancedMarkerElement({ map: this.map, ...options });
       });
   }
