51288579fa2a11bb53df3731f5a912c587471542
Minister <mundestephane13@gmail.com>
2025-01-12 11:02:09 +0100

    fixed icons issues

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -104,75 +104,83 @@ export class HomePage implements AfterViewInit {
     });
 
     effect(() => {
-      if (this.selectedLocations().length > 0) {
-        const center = {
-          max: {
-            lat: -Infinity,
-            lng: -Infinity,
-          },
-          min: {
-            lat: Infinity,
-            lng: Infinity,
-          },
-        };
+      if (isPlatformBrowser(this.platformId)) {
+        if (this.selectedLocations().length > 0) {
+          const center = {
+            max: {
+              lat: -Infinity,
+              lng: -Infinity,
+            },
+            min: {
+              lat: Infinity,
+              lng: Infinity,
+            },
+          };
 
-        this.selectedLocations().forEach((location) => {
-          if (location.coordinate.lat >= center.max.lat) {
-            center.max.lat = location.coordinate.lat;
-          }
+          this.selectedLocations().forEach((location) => {
+            if (location.coordinate.lat >= center.max.lat) {
+              center.max.lat = location.coordinate.lat;
+            }
 
-          if (location.coordinate.lng >= center.max.lng) {
-            center.max.lng = location.coordinate.lng;
-          }
+            if (location.coordinate.lng >= center.max.lng) {
+              center.max.lng = location.coordinate.lng;
+            }
 
-          if (location.coordinate.lat <= center.min.lat) {
-            center.min.lat = location.coordinate.lat;
-          }
+            if (location.coordinate.lat <= center.min.lat) {
+              center.min.lat = location.coordinate.lat;
+            }
 
-          if (location.coordinate.lng <= center.min.lng) {
-            center.min.lng = location.coordinate.lng;
-          }
+            if (location.coordinate.lng <= center.min.lng) {
+              center.min.lng = location.coordinate.lng;
+            }
 
-          if (this.selectedLocations()?.length == 1) {
-            center.max = {
-              lng: location.coordinate.lng,
-              lat: location.coordinate.lng,
-            };
-            center.min = {
-              lng: location.coordinate.lng,
-              lat: location.coordinate.lng,
-            };
-          }
+            if (this.selectedLocations()?.length == 1) {
+              center.max = {
+                lng: location.coordinate.lng,
+                lat: location.coordinate.lng,
+              };
+              center.min = {
+                lng: location.coordinate.lng,
+                lat: location.coordinate.lng,
+              };
+            }
 
-          this.markers.forEach((marker) => {
-            marker.map = null;
+            this.markers.forEach((marker) => {
+              marker.map = null;
+            });
+
+            const category =
+              categories[location.category as keyof typeof categories] ??
+              categories['default'];
+            const glyph = document.createElement('div');
+            glyph.classList.add('map-glyph');
+            glyph.style.backgroundColor = `color-mix(in oklab, var(--color-${category?.color}) 50%, transparent)`;
+            glyph.innerHTML = `<i class="${category?.icon} text-lg pt-px" style="color: var(--color-white)" role="img" aria-hidden="true"></i>`;
+
+            this.mapService
+              .addAdvancedMarker({
+                position: location.coordinate,
+                title: location.name,
+                pin: {
+                  glyph,
+                  glyphColor: 'yellow',
+                  background: 'transparent',
+                  borderColor: 'transparent',
+                },
+              })
+              .then((marker) => this.markers.push(marker));
+
+            // this.mapService.showDirection(
+            //   this.currentLocation() as google.maps.LatLngLiteral,
+            //   location.coordinate,
+            // );
           });
 
-          const category =
-            categories[location.category as keyof typeof categories];
-          const glyph = document.createElement('div');
-          glyph.classList.add('map-glyph');
-          glyph.style.backgroundColor = `color-mix(in oklab, var(--color-${category.color}) 50%, transparent)`;
-          glyph.innerHTML = `<i class="${category.icon} text-lg pt-px" style="color: var(--color-white)" role="img" aria-hidden="true"></i>`;
-
-          this.mapService
-            .addAdvancedMarker({
-              position: location.coordinate,
-              title: location.name,
-              pin: {
-                glyph,
-                glyphColor: 'yellow',
-                background: 'transparent',
-                borderColor: 'transparent',
-              },
-            })
-            .then((marker) => this.markers.push(marker));
-        });
-
-        this.mapService.setCenter({
-          lat: (center.max.lat + center.min.lat) / 2,
-          lng: (center.max.lng + center.min.lng) / 2,
-        });
+          this.mapService.setCenter({
+            lat: (center.max.lat + center.min.lat) / 2,
+            lng: (center.max.lng + center.min.lng) / 2,
+          });
+        }
       }
     });
   }
@@ -180,6 +188,7 @@ export class HomePage implements AfterViewInit {
   ngOnInit() {
     this.mapService.loadMap({
       id: 'UB',
+      libraries: ['places', 'marker'],
     });
   }
 
