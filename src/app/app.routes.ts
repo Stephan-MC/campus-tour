091cc52cc25deaaaf04a13496c3bd5b09a7ef202
Minister <mundestephane13@gmail.com>
2025-01-12 11:01:56 +0100

    Added a convinient title to the project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
+    title: 'UB Campus Tourism',
     loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
   },
 ];
